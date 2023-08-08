0e0c7547bffd1fe7ea9b614f828a21edac9228f7
rkhudov <rhudov@gmail.com>
2023-08-08 12:29:06 +0100

    Add documentation fot btf_types

--- btf_types/src/lib.rs
+++ btf_types/src/lib.rs
@@ -1,26 +1,36 @@
-use std::fs;
 use std::error::Error;
+use std::fs;
 use std::path::Path;
 
+/// Provide enum for all possible BF language instructions.
 #[derive(Debug)]
 pub enum RawInstructions {
-    IncrementDataPointer, // <
-    DecrementDataPointer, // >
-    IncrementByte,  // +
-    DecrementByte,  // -
-    OutputByte,  // .
-    AcceptByte,  // ,
-    ZeroJump,  // [
-    NonZeroJump,  // ]
+    /// Reprsent `>` symbol. Increment the data pointer by one (to point to the next cell to the right).
+    IncrementDataPointer,
+    /// Reprsent `<` symbol. Decrement the data pointer by one (to point to the next cell to the left).
+    DecrementDataPointer,
+    /// Reprsent `+` symbol. Increment the byte at the data pointer by one.
+    IncrementByte,
+    // Reprsent `-` symbol. Decrement the byte at the data pointer by one.
+    DecrementByte,
+    /// Reprsent `.` symbol. Output the byte at the data pointer.
+    OutputByte,
+    /// Reprsent `,` symbol. Accept one byte of input, storing its value in the byte at the data pointer.
+    AcceptByte,
+    /// Reprsent `[` symbol. If the byte at the data pointer is zero, then instead of moving the instruction pointer forward to the next command, jump it forward to the command after the matching ] command.
+    ZeroJump,
+    /// Reprsent `]` symbol. If the byte at the data pointer is nonzero, then instead of moving the instruction pointer forward to the next command, jump it back to the command after the matching [ command.
+    NonZeroJump,
 }
 
+/// Try to convert char into BF language instruction.
 impl TryFrom<char> for RawInstructions {
     type Error = &'static str;
 
     fn try_from(symbol: char) -> Result<RawInstructions, Self::Error> {
         match symbol {
-            '<' => Ok(RawInstructions::IncrementDataPointer),
-            '>' => Ok(RawInstructions::DecrementDataPointer),
+            '<' => Ok(RawInstructions::DecrementDataPointer),
+            '>' => Ok(RawInstructions::IncrementDataPointer),
             '+' => Ok(RawInstructions::IncrementByte),
             '-' => Ok(RawInstructions::DecrementByte),
             '.' => Ok(RawInstructions::OutputByte),
@@ -32,13 +42,17 @@ impl TryFrom<char> for RawInstructions {
     }
 }
 
+/// Provide structure to reprsent BF program.
 #[derive(Debug)]
 pub struct BrainFuckProgram {
+    /// Name of the file from where program is parsed.
     filename: String,
+    /// List of instructions parsed from file.
     instructions: Vec<RawInstructions>,
 }
 
 impl BrainFuckProgram {
+    /// Create BF program based on the name of the file and it's content.
     fn new(filename: String, content: String) -> BrainFuckProgram {
         let mut instructions: Vec<RawInstructions> = Vec::new();
         for char in content.chars() {
@@ -49,14 +63,27 @@ impl BrainFuckProgram {
                 Err(_e) => {}
             }
         }
-        BrainFuckProgram {filename, instructions}
+        BrainFuckProgram {
+            filename,
+            instructions,
+        }
     }
 
-    fn from_file<T: AsRef<Path>>(file_path: T) -> Result<(), Box<dyn Error>> {
+    /// Parse BF program from file.
+    pub fn from_file<T: AsRef<Path>>(file_path: T) -> Result<BrainFuckProgram, Box<dyn Error>> {
         let file_path_ref = file_path.as_ref();
         let content = fs::read_to_string(file_path_ref)?;
         let bf_program = Self::new(file_path_ref.to_string_lossy().to_string(), content);
-        println!("{:?}", bf_program);
-        Ok(())
+        Ok(bf_program)
+    }
+
+    /// Get name of the file from where BF program is parsed.
+    pub fn filename(&self) -> &String {
+        &self.filename
+    }
+
+    /// Get list of instructions for BF program.
+    pub fn instructions(&self) -> &[RawInstructions] {
+        &self.instructions[..]
     }
 }

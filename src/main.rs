6314c227520ca26f4eb4a8803c0011e49d9b4cbd
rkhudov <rhudov@gmail.com>
2023-08-09 14:53:01 +0100

    init

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
 //! Provide implementation of parsing BF program.
+use argh::FromArgs;
 use btf_interp::VirtualMachine;
 use btf_types::BrainFuckProgram;
-use std::env::args_os;
 use std::error::Error;
+use std::path::PathBuf;
 
+#[derive(FromArgs)]
+#[argh(description = "brainfuck program parser", name = "bft")]
+struct Args {
+    #[argh(option, description = "brainfuck program to be parsed.")]
+    program: PathBuf,
+}
 fn main() -> Result<(), Box<dyn Error>> {
-    let file_path = args_os().nth(1).expect("Please, provide file path.");
+    let args: Args = argh::from_env();
+    let file_path = args.program;
+    // let file_path = args_os().nth(1).expect("Please, provide file path.");
     let bf_program = BrainFuckProgram::from_file(file_path);
     let vm: VirtualMachine<u8> = VirtualMachine::new(None, None);
     match bf_program {
